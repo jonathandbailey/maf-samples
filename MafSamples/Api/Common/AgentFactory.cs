dfa47812a16e707de7500bc2efb756a3d8dbdf5c
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2026-01-25 14:06:43 +0100

    Adding Basic AGUI agent with swagger

---
+++ MafSamples/Api/Common/AgentFactory.cs
@@ -0,0 +1,40 @@
+﻿using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Options;
+using System.ClientModel;
+
+namespace Api.Common;
+
+public class AgentFactory(IOptions<LanguageModelSettings> settings) : IAgentFactory
+{
+    public async Task<AIAgent> Create()
+    {
+        var chatClient = new AzureOpenAIClient(new Uri(settings.Value.EndPoint),
+                new ApiKeyCredential(settings.Value.ApiKey))
+            .GetChatClient(settings.Value.DeploymentName);
+
+        ChatOptions chatOptions = new()
+        {
+            Instructions = "You are a helpful assistant that answers questions."
+        };
+
+        var clientChatOptions = new ChatClientAgentOptions
+        {
+            Name = "Assistant",
+
+            ChatOptions = chatOptions
+        };
+
+        var agent = chatClient.AsIChatClient()
+            .AsBuilder()
+            .BuildAIAgent(options: clientChatOptions);
+
+        return await Task.FromResult(agent);
+    }
+}
+
+public interface IAgentFactory
+{
+    Task<AIAgent> Create();
+}
--- MafSamples/Api/Program.cs
+++ MafSamples/Api/Program.cs
@@ -1,8 +1,19 @@
+using Api.Common;
+using Microsoft.Agents.AI.Hosting.AGUI.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.Services.Configure<LanguageModelSettings>(settings =>
+    builder.Configuration.GetSection("LanguageModelSettings").Bind(settings));
+
+builder.Services.AddSingleton<IAgentFactory, AgentFactory>();
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -10,10 +21,18 @@ app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+var agentFactory = app.Services.GetRequiredService<IAgentFactory>();
+
+var agent = await agentFactory.Create();
+
+app.MapAGUI("/", agent);
 
 app.Run();
 
